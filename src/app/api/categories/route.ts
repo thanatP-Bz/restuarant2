d5127c0b95681251f04fbeb96fac6d154f31d868
Thanat <bztzn512@gmail.com>
2024-07-29 17:32:43 -0400

    resturantAndCompletedDatabaseConnection

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new Response(JSON.stringify(categories), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = () => {
+  return new NextResponse("HELLO", { status: 2000 });
+};
